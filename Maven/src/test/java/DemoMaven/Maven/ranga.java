cd9cfaccc882556805a30de390f2f0ff8bbd74ce
prakashmanohara <prakash.manohara@gmail.com>
2018-11-19 19:31:18 +0530

    updated the script with sop

--- Maven/src/test/java/DemoMaven/Maven/ranga.java
+++ Maven/src/test/java/DemoMaven/Maven/ranga.java
@@ -9,13 +9,15 @@ public class ranga {
 	public static void main(String[] args) {
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/prakashm/Documents/Prakash/Automation/MaximCrane/Drivers/chromedriver");
-		WebDriver driver1=new ChromeDriver();
-		driver1.get("https://www.facebook.com/");
-		driver1.findElement(By.id("email")).sendKeys("test@gmail.com");
-
-		driver1.findElement(By.id("pass")).sendKeys("Test");
-
-		driver1.findElement(By.id("u_0_2")).click();
+		
+		System.out.println("Testing the ranga");
+		//WebDriver driver1=new ChromeDriver();
+//		driver1.get("https://www.facebook.com/");
+//		driver1.findElement(By.id("email")).sendKeys("test@gmail.com");
+//
+//		driver1.findElement(By.id("pass")).sendKeys("Test");
+//
+//		driver1.findElement(By.id("u_0_2")).click();
 		
 		}
 	
